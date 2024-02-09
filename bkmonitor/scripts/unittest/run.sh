b10885b98401718d8999aaf95f65745201848df7
crayon <873217631@qq.com>
2024-02-09 16:41:11 +0800

    test: add test

--- bkmonitor/scripts/unittest/run.sh
+++ bkmonitor/scripts/unittest/run.sh
@@ -4,11 +4,12 @@ mysql -e 'CREATE DATABASE `bk_monitor_saas` DEFAULT CHARACTER SET utf8mb4 COLLAT
 mysql -e 'CREATE DATABASE `bk_monitor_api` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;'
 
 env DJANGO_CONF_MODULE="" BKAPP_DEPLOY_ENV="web" python manage.py migrate
+env DJANGO_CONF_MODULE="" BKAPP_DEPLOY_ENV="web" python manage.py migrate
 
-pytest alarm_backends/tests > pytest.log
+pytest alarm_backends/tests 2>&1 | tee pytest.log
 
 python scripts/unittest/parse_test_output.py "$(pwd)/pytest.log"
 
-python manage.py test alarm_backends.tests > testcase.log
+python manage.py test alarm_backends.tests 2>&1 | tee testcase.log
 
 python scripts/unittest/parse_test_output.py "$(pwd)/testcase.log"
